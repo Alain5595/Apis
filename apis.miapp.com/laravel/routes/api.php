6cf3113fefed8870e0dd786ba23d52c25f238189
Alain Peralta <104697852+Alain5595@users.noreply.github.com>
2022-06-22 14:35:10 -0500

    Update api.php

--- apis.miapp.com/laravel/routes/api.php
+++ apis.miapp.com/laravel/routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\v1\PacienteController;
+use App\Http\Controllers\v1\SecurityController;
+use App\Http\Controllers\v1\UsersController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +22,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 */
+Route::post('/v1/seguridad/login',[SecurityController::class,'login']);
+Route::post('/v1/users',[UsersController::class,'users']);
 
 Route::get('/v1/paciente',[PacienteController::class,'getAll']);
 Route::get('/v1/paciente/{id}',[PacienteController::class,'getItem']);
@@ -26,3 +31,7 @@ Route::post('/v1/paciente',[PacienteController::class,'store']);
 Route::put('/v1/paciente',[PacienteController::class,'putUpdate']);
 Route::patch('/v1/paciente',[PacienteController::class,'patchUpdate']);
 Route::delete('/v1/paciente/{id}',[PacienteController::class,'delete']);
+
+Route::middleware(['auth:api'])->group(function () {
+
+});
